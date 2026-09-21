d0a753093dc45fdb42add58de18ec485ace628ba
Bless <rukundobless47@gmail.com>
2026-09-21 11:13:32 +0200

    4kyu: This use BigInt to receive the biggest numbers and return thier sum in form of string also as it recieve it in form of string

---
+++ (4 kyu) Sum Strings as Numbers.js
@@ -0,0 +1,8 @@
+function sumStrings(a,b) { 
+    let first = Number(a);
+    let second = Number(b);
+    let sum = first+second;
+    return sum.toString();
+
+}
+console.log(sumStrings('123','456'))
